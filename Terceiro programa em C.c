52b3fbe55f047c511e228eab055f63dd38fce3d7
Etelvina Torres <165731947+ettorres12@users.noreply.github.com>
2025-01-24 11:53:31 -0300

    Add files via upload

---
+++ Terceiro programa em C.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+	int main (){
+		float valor;
+		float valor2;
+		float media;
+			printf("Digite o primeiro valor: ");
+			scanf("%f", &valor);
+			printf("Digite o segundo valor: ");
+			scanf("%f", &valor2);
+				media = (valor + valor2)/2.0;
+					printf("O valor da media e: %.2f", media);
+					return 0;
+			
+	}
